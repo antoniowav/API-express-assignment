86caf3216ba020331494ac46d481582e39702437
antoniowav <85946713+antoniowav@users.noreply.github.com>
2022-08-31 10:23:35 +0200

    Latest changes and final touches

--- client/savedJokes.js
+++ client/savedJokes.js
@@ -1,6 +1,23 @@
-fetch("http://localhost:3000/jokes/saved").then((response) => {
-            return response.json()
-        }).then((data) => {
-            console.log(data);
-            document.getElementById('savedJoke').innerHTML = data
-        })
+fetch("http://localhost:3000/jokes/saved")
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    console.log(data);
+
+    if (data.length === 0) {
+
+      console.log("no jokes");
+      document.getElementById("jokesContainer").innerHTML = "No jokes saved!";
+
+    } else {
+
+      let renderedHtml = data.map(function (item) {
+        return `<div id="singleJoke"><h1>${item.setup}</h1> <h1>${item.delivery}</h1></div><br>`;
+
+      });
+      
+      document.getElementById("jokesContainer").innerHTML =
+        renderedHtml.join(" ");
+    }
+  });
--- server.js
+++ server.js
@@ -1,55 +1,38 @@
-import express from "express"
+import express from "express";
 
 import fetch from "node-fetch";
 
-
-const app = express()
-const port = 3000
-
+const app = express();
+const port = 3000;
+app.use(express.json());
 
 app.listen(port, () => {
-    console.log("App is running on port" + port)
-})
+  console.log("App is running on port" + port);
+});
 
-
-app.use('/', express.static('client'))
-
+app.use("/", express.static("client"));
 
 app.get("/jokes", async (req, res) => {
-    
-    const response = await fetch("https://v2.jokeapi.dev/joke/Programming,Miscellaneous,Spooky,Christmas?blacklistFlags=religious,political,explicit&type=twopart")
-    const data = await response.json()
-    res.json(data)
-})
+  const response = await fetch(
+    "https://v2.jokeapi.dev/joke/Programming,Miscellaneous,Spooky,Christmas?blacklistFlags=religious,political,explicit&type=twopart"
+  );
+  const data = await response.json();
+  res.json(data);
+});
 
+let savedJokes = [];
 
-let savedJokes = []
+app.get("/jokes/saved", (req, res) => {
+  try {
+    res.send(savedJokes);
+  } catch (err) {
+    res.status(500).json(err.message);
+  }
+});
 
-app.get('/jokes/saved', (req, res) => {
-    
-    try {
-        res.json(savedJokes)
-    } catch(err) {
-        res.status(500).json(err.message)
-    }
-})
-
-app.post('/jokes/saved', (req, res) => {
-    
-    let newJoke = JSON.stringify(req.body)
-    savedJokes.push(newJoke)
-    res.send(savedJokes)
-    console.log(req.body)
-
-
-})
-
-
-
-
-
-
-
-
-
-
+app.post("/jokes/saved", (req, res) => {
+  let newJoke = req.body;
+  savedJokes.push(newJoke);
+  res.send(savedJokes);
+  console.log(savedJokes);
+});
